42d26d48bd5911a3413c6fad7cc1dbdcae26cab5
snow <smashdojo3@gmail.com>
2022-06-06 23:03:09 -0400

    se crea direccion api

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const productoController = require ('../controller/productoController');
 
-router.get('/api/v0/searchProduct/:id', controller.getUser)
-router.get('/api/v0/searchallProduct',controller.getAll)
+router.get('/api/v0/searchcategory/:id', productoController.searchCategory)
+router.get('/api/v0/searchallproduct',productoController.searchAllProduct)
 
 module.exports = router;
